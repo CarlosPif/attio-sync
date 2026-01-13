158d12072967b40296acc7a08bbc8edde0db4902
Carlos Martinez Piferrer <carlos@decelera.com>
2026-01-13 11:13:05 +0000

    u

--- app/database.py
+++ app/database.py
@@ -4,14 +4,23 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-load_dotenv()
-# Vamos con la url de la base
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
+def get_engine():
+    url = os.getenv("DATABASE_URL")
+
+    if not url:
+        raise RuntimeError("❌ ERROR CRÍTICO: DATABASE_URL no encontrada en el sistema.")
+
+    return create_engine(url)
+
+engine = None
+SessionLocal = None
+
 def init_db():
-    # Esto crea las tablas si no existen
+    global engine, SessionLocal
+    engine = get_engine()
+    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+    from app.models import Company, FastTrack
     Base.metadata.create_all(bind=engine)
